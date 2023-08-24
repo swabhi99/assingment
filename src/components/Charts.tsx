e7baf17a77900aac01091fdc1d413656b921383b
swabhi99 <smtsingh538@gmail.com>
2023-08-25 02:43:51 +0530

    first

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -3,6 +3,7 @@ import { Box, Flex, Heading } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Chart } from 'chart.js/dist';
 import { Chart as ChartJS } from "chart.js/auto";
 import "leaflet/dist/leaflet.css"
 const fetchChartData = async () => {
